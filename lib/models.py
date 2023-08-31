9dc9e559b174ea4a035111b448119e3a97a3f3f0
Kasembeli8865 <lionel.kasembeli@student.moringaschool.com>
2023-08-31 22:21:11 +0300

    final commit

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,61 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+# Create an SQLAlchemy engine and session
+engine = create_engine('sqlite:///theater.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String())
+
+    # Relationship to Auditions
+    auditions = relationship('Audition', back_populates='role')
+
+    def __repr__(self):
+        return f'<Role {self.character_name}>'
+
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        hired_audition = next((audition for audition in self.auditions if audition.hired), None)
+        if hired_audition:
+            return hired_audition
+        else:
+            return "no actor has been hired for this role"
+
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if len(hired_auditions) >= 2:
+            return hired_auditions[1]
+        else:
+            return "no actor has been hired for understudy for this role"
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String())
+    location = Column(String())
+    hired = Column(Boolean, default=False)
+
+    role_id = Column(Integer, ForeignKey('roles.id'))
+    role = relationship('Role', back_populates='auditions')
+
+    def __repr__(self):
+        return f'<Audition {self.actor} for {self.role.character_name}>'
+
+    def call_back(self):
+        self.hired = True
+        session.commit()
